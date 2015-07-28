2b0e75a6c37ca7751e027be519429c113f64596e
Sungsit Sawaiwan <sungsit@users.noreply.github.com>
2015-07-28 21:59:48 +0700

    #8 New testing pages

--- js/boontook.js
+++ js/boontook.js
@@ -35,9 +35,13 @@ for (var i = 0, len = texts.length; i<len; i++) {
   texts[i].nodeValue = _nv.replace(glyphReplace, function(matched){return glyphObj[matched]});
   
   // debugging
-  console.log(texts[i].nodeValue);
+  //console.log(texts[i].nodeValue);
 }
 
-// To-do:
-// - Check Thanthakhat vs Yamakkan transliterate for writing English with Thai script http://www.bflybook.com/Article/Yamakkan/Yamakkan.htm.
-// - OT `locl` feature for `lang="en-TH"` for example.
+function fontOption(){
+  fontselector = document.getElementById('font-selector');
+  fontcanvas = document.getElementsByTagName('body')[0];
+  fontid = fontselector.options[fontselector.selectedIndex].value;
+  fontcanvas.className = '';
+  fontcanvas.className = fontid;
+}
