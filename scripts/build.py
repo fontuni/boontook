d5170115e82ac2d247119fe4a4fd03621ac19df7
Sungsit Sawaiwan <sungsit@users.noreply.github.com>
2015-12-02 01:31:27 +0700

    Correct PS private values & new auto-hinting

--- scripts/build.py
+++ scripts/build.py
@@ -14,11 +14,12 @@ import subprocess
 import shutil
 import time
 import datetime
+import glob
 
 # Predifined vars
 foundry = 'FontUni'
 family = 'BoonTook'
-version = '2.0'
+version = '2.0.1'
 weights = [950]
 sources = ['sources/boontook-roman.sfd','sources/boontook-oblique.sfd']
 layers = ['Normal', 'Mon']
@@ -38,7 +39,12 @@ unhinted_dir = build_dir + 'unhinted/'
 if not os.path.exists(unhinted_dir):
   os.makedirs(unhinted_dir)
 
-exts = ['otf', 'ttf', 'woff', 'woff2']
+def fontPath(ext,name):
+  path = build_dir + ext
+  if not os.path.exists(path):
+    os.makedirs(path)
+  fontfile = path + '/' + name + '.' + ext
+  return fontfile
 
 def printFontInfo(fontfile):
   font = fontforge.open(fontfile)
@@ -48,66 +54,41 @@ def printFontInfo(fontfile):
   print('Full Name: ' + font.fullname)
   print('Font Weight: ' + font.weight)
   print('OS2 Weight: ' + str(font.os2_weight))
-  print('Italic Angle: ' + str(font.italicangle))
+  print('Oblique Angle: ' + str(font.italicangle))
   print('Font Version: ' + font.version)
   print('Font Copyright: ' + font.copyright)
   font.close()
 
-def ttfHint(unhinted,hinted):
-  subprocess.call([
-    'ttfautohint',
-    '--default-script=thai',
-    '--fallback-script=latn',
-    '--strong-stem-width=G',
-    '--hinting-range-min=7',
-    '--hinting-range-max=28',
-    '--hinting-limit=50',
-    '--increase-x-height=13',
-    '--no-info',
-    '--verbose',
-    unhinted,
-    hinted
-  ])
-
-# Optimize
-def fontOptimize(fontfile):
-  subprocess.call([
-    'pyftsubset',
-    fontfile,
-    '--glyphs=*',
-    '--layout-features=*',
-    '--name-IDs=*',
-    '--hinting',
-    '--legacy-kern',
-    '--notdef-outline',
-    '--no-subset-tables+=DSIG',
-    '--drop-tables-=DSIG',
-    '--output-file=' + fontfile
-  ])
-
-def ttf2Woff(ttf,woff,genflags):
-  font = fontforge.open(ttf)
-  font.generate(woff, flags=genflags)
-  font.close()
-
-def fontPath(ext,name):
-  path = build_dir + ext
-  if not os.path.exists(path):
-    os.makedirs(path)
-  fontfile = path + '/' + name + '.' + ext
-  return fontfile
-
 def otf2Sfd(otf,sfd_dir):
+  
   font = fontforge.open(otf)
   sfd = sfd_dir + font.fontname + '.sfd'
   if not os.path.exists(sfd_dir):
     os.makedirs(sfd_dir)
+
   font.appendSFNTName('English (US)', 'UniqueID', '')
+  
+  weight = font.os2_weight
+  font.private['BlueValues'] = (-20, 0, 600, 620, 840, 860)
+  font.private['OtherBlues'] = (-260, -240)
+  font.private['BlueShift'] = 0,
+  font.private['StdHW'] = 180,
+  font.private['StdVW'] = 280,
+  font.private['StemSnapH'] = (90, 120, 140, 150, 180, 200, 210, 240)
+  font.private['StemSnapV'] = (270, 280)
+  font.selection.all()
+  font.autoHint()
   font.save(sfd)
-  print(font.fontname, 'SFD files saved.')
+  font.private.guess('StemSnapH')
+  font.private.guess('StemSnapV')
+  font.private.guess('BlueShift')
+  font.selection.all()
+  font.autoHint()
+  font.save(sfd)
+  print(sfd, 'file saved.')
   font.close()
 
-def buildFont(source,family):
+def buildSFD(source,family):
 
   # prepare master
   font = fontforge.open(source)
@@ -116,7 +97,7 @@ def buildFont(source,family):
   font.fullname = font.fontname.replace('-',' ')
   font.appendSFNTName('English (US)', 'Manufacturer', foundry)
   font.appendSFNTName('English (US)', 'Preferred Family', family)
-  font.os2_weight = 950
+  font.os2_weight = 400
   style = 'Ultra'
   font.version = version
   font.copyright = copyright
@@ -186,31 +167,129 @@ def buildFont(source,family):
     # save sfd
     otf2Sfd(otf,sfd_dir)
 
-    # generate unhinted ttf
-    ttfgenflags  = ('opentype', 'no-hints')
-    ttfunhinted = unhinted_dir + font.fontname + '-unhinted.ttf'
-    font.generate(ttfunhinted, flags=ttfgenflags, layer = layer)
-    print(font.fullname, 'Unhinted TTF instance generated.')
-
-    # ttfautohint
-    ttfHint(ttfunhinted,ttf)
-    fontOptimize(ttf)
-    printFontInfo(ttf)
-    print(font.fullname, 'TTF autohinted.')
-
-    # hinted ttf to woff
-    ttf2Woff(ttf,woff,ttfgenflags)
-    print(font.fullname, 'WOFF instance generated.')
-
-    # hinted ttf to woff2
-    subprocess.call(['woff2_compress',ttf])
-    os.rename(tempwoff2, woff2)
-    print(font.fullname, 'WOFF2 instance generated.')
-
   font.close()
 
 for source in sources:
-  buildFont(source,family)
+  buildSFD(source,family)
+
+# Generate & hint SFD files
+
+exts = ['otf', 'ttf', 'woff', 'woff2', 'eot', 'svg']
+
+def otfHint(unhinted,hinted):
+  subprocess.call([
+    'autohint',
+    '-q',
+    '-o',
+    hinted,
+    unhinted
+  ])
+  print(hinted, 'autohinted.')
+
+def ttfHint(unhinted,hinted):
+  subprocess.call([
+    'ttfautohint',
+    '--default-script=latn',
+    '--fallback-script=thai',
+    '--strong-stem-width=G',
+    '--hinting-range-min=7',
+    '--hinting-range-max=28',
+    '--hinting-limit=50',
+    '--increase-x-height=13',
+    '--no-info',
+    '--verbose',
+    unhinted,
+    hinted
+  ])
+  print(hinted, 'autohinted.')
+
+# Optimize
+def fontOptimize(fontfile):
+  subprocess.call([
+    'pyftsubset',
+    fontfile,
+    '--glyphs=*',
+    '--layout-features=*',
+    '--name-IDs=*',
+    '--hinting',
+    '--legacy-kern',
+    '--notdef-outline',
+    '--no-subset-tables+=DSIG',
+    '--drop-tables-=DSIG',
+    '--output-file=' + fontfile
+  ])
+  print(fontfile, 'optimized.')
+
+# http://wizard.ae.krakow.pl/~jb/ttf2woff/
+def ttf2Woff(ttf,woff):
+  subprocess.call([ 'ttf2woff', '-v', ttf, woff ])
+  print(woff, 'instance generated.')
+
+def ttf2Woff2(ttf,woff2):
+  subprocess.call(['woff2_compress',ttf])
+  (root, ext) = os.path.splitext(ttf)
+  os.rename(root + '.woff2', woff2)
+  print(woff2, 'instance generated.')
+
+def ttf2Eot(ttf,eot):
+  subprocess.call(['ttf2eot',ttf,eot])
+  print(eot, 'instance generated.')
+
+def buildFont(sfd):
+  font = fontforge.open(sfd)
+
+  # UniqueID with timestamp
+  ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+  uniqueid = foundry + ' : ' + font.fullname + ' ' + font.version + ' : ' + ts
+  font.appendSFNTName('English (US)', 'UniqueID', uniqueid)
+
+  otf = fontPath('otf',font.fontname)
+  ttf = fontPath('ttf',font.fontname)
+  woff = fontPath('woff',font.fontname)
+  woff2 = fontPath('woff2',font.fontname)
+  eot = fontPath('eot',font.fontname)
+  svg = fontPath('svg',font.fontname)
+  tempwoff2 = build_dir + 'ttf/' + font.fontname + '.woff2'
+
+  # generate otf
+  otfgenflags  = ('opentype', 'PfEd-lookups')
+  otfunhinted = unhinted_dir + font.fontname + '-unhinted.otf'
+  font.generate(otfunhinted, flags=otfgenflags)
+  print(otfunhinted, 'instance generated.')
+
+  # AFDKO autohint
+  otfHint(otfunhinted,otf)
+  fontOptimize(otf)
+  printFontInfo(otf)
+
+  # generate unhinted ttf
+  ttfgenflags  = ('opentype', 'no-hints')
+  ttfunhinted = unhinted_dir + font.fontname + '-unhinted.ttf'
+  font.generate(ttfunhinted, flags=ttfgenflags)
+  print(ttfunhinted, 'instance generated.')
+
+  # ttfautohint
+  ttfHint(ttfunhinted,ttf)
+  fontOptimize(ttf)
+  printFontInfo(ttf)
+
+  # ttf2woff
+  ttf2Woff(ttf,woff)
+
+  # ttf2woff2
+  ttf2Woff2(ttf,woff2)
+
+  # ttf2eot
+  ttf2Eot(ttf,eot)
+
+  # gen svg
+  font.generate(svg, flags=otfgenflags)
+  print(svg, 'instance generated.')
+
+  font.close()
+
+for sfd in sorted(glob.glob('./sfd/*.sfd')):
+  buildFont(sfd)
 
 # Create zip package for each font extension
 def fontZip(family,version,ext):
@@ -222,6 +301,8 @@ def fontZip(family,version,ext):
   os.remove(ext + '/OFL.txt')
   os.chdir('..')
   print(package, 'created.')
+
+exts = ['otf', 'ttf', 'woff', 'woff2', 'eot', 'svg']
 
 for ext in exts:
   fontZip(family,version,ext)
